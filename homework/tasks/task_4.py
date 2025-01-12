1cbb926c070713b7e2d75f9382058804001c4413
Vlada Stikharnaya <Vlada_Stikharnaya@mail.ru>
2025-01-12 23:32:18 +0500

    done

--- homework/tasks/task_4.py
+++ homework/tasks/task_4.py
@@ -1,4 +1,6 @@
 async def task_1(i: int):
+    global result
+    result.append(1)
     if i == 0:
         return
 
@@ -9,6 +11,8 @@ async def task_1(i: int):
 
 
 async def task_2(i: int):
+    global result
+    result.append(2)
     if i == 0:
         return
 
@@ -27,6 +31,7 @@ async def coroutines_execution_order(i: int = 42) -> int:
     # Пример:
     # i = 7
     # return 12212
+    global result
+    result = []
     await task_1(i)
-
-    # YOUR CODE GOES HERE
+    return int(''.join(map(str, result)))
